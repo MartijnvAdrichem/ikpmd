8dd26b72b455f01ce30f37226df246d2557205ec
Gerrit Louis <gerritlouis@gmail.com>
2018-05-23 16:55:47 +0200

    Login and register works!

---
+++ app/src/main/java/com/example/marti/pocketbattle/HomeScreenView.java
@@ -0,0 +1,32 @@
+package com.example.marti.pocketbattle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+public class HomeScreenView extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextView hsText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen_view);
+        mAuth = FirebaseAuth.getInstance();
+        hsText = (TextView) findViewById(R.id.hsText);
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        hsText.setText(currentUser.getEmail().toString());
+    }
+}
